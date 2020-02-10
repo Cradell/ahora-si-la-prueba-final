3c1717a0f56c482bd15eea148885b79cd5e111a9
tomas olivares <tomas3105@gmail.com>
2020-02-10 19:55:01 -0300

    cambios generales

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
-$(function() {
-  $("a").click(function(event) {
-
-    if (this.hash !== "nav-link") {
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
       event.preventDefault();
-      var gato = this.hash;
+      var hash = this.hash;
       $('html, body').animate({
-        scrollTop: $(gato).offset().top
-      }, 800, function() {
-        window.location.hash = gato;
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
       });
     }
   });
